a66ece3d926a84e5e31bade1a463bf2f07d88aa9
23120231 <23120231@studentmail.ul.ie>
2024-07-17 21:59:05 +0100

    GOD CLASS AND FEATURE ENVY

--- src/main/java/org/mybatis/jpetstore/service/AccountService.java
+++ src/main/java/org/mybatis/jpetstore/service/AccountService.java
@@ -19,6 +19,12 @@ import java.util.Optional;
 
 import org.mybatis.jpetstore.domain.Account;
 import org.mybatis.jpetstore.mapper.AccountMapper;
+import org.mybatis.jpetstore.mapper.CategoryMapper;
+import org.mybatis.jpetstore.mapper.ItemMapper;
+import org.mybatis.jpetstore.mapper.LineItemMapper;
+import org.mybatis.jpetstore.mapper.OrderMapper;
+import org.mybatis.jpetstore.mapper.ProductMapper;
+import org.mybatis.jpetstore.mapper.SequenceMapper;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -27,49 +33,188 @@ import org.springframework.transaction.annotation.Transactional;
  *
  * @author Eduardo Macarron
  */
+
+ @Service
+ public class AccountService {
+     private final AccountMapper accountMapper;
+     private final CategoryMapper categoryMapper;
+     private final ItemMapper itemMapper;
+     private final ProductMapper productMapper;
+     private final OrderMapper orderMapper;
+     private final SequenceMapper sequenceMapper;
+     private final LineItemMapper lineItemMapper;
+
+     public AccountService(AccountMapper accountMapper, CategoryMapper categoryMapper, ItemMapper itemMapper, ProductMapper productMapper, OrderMapper orderMapper, SequenceMapper sequenceMapper, LineItemMapper lineItemMapper) {
+         this.accountMapper = accountMapper;
+         this.categoryMapper = categoryMapper;
+         this.itemMapper = itemMapper;
+         this.productMapper = productMapper;
+         this.orderMapper = orderMapper;
+         this.sequenceMapper = sequenceMapper;
+         this.lineItemMapper = lineItemMapper;
+     }
+
+     // Account methods
+     public Account getAccount(String username) {
+         return accountMapper.getAccountByUsername(username);
+     }
+
+     public Account getAccount(String username, String password) {
+         return accountMapper.getAccountByUsernameAndPassword(username, password);
+     }
+
+     @Transactional
+     public void insertAccount(Account account) {
+         accountMapper.insertAccount(account);
+         accountMapper.insertProfile(account);
+         accountMapper.insertSignon(account);
+     }
+
+     @Transactional
+     public void updateAccount(Account account) {
+         accountMapper.updateAccount(account);
+         accountMapper.updateProfile(account);
+         Optional.ofNullable(account.getPassword()).filter(password -> password.length() > 0).ifPresent(password -> accountMapper.updateSignon(account));
+     }
+
+     // Catalog methods
+     public List<Category> getCategoryList() {
+         return categoryMapper.getCategoryList();
+     }
+
+     public Category getCategory(String categoryId) {
+         return categoryMapper.getCategory(categoryId);
+     }
+
+     public Product getProduct(String productId) {
+         return productMapper.getProduct(productId);
+     }
+
+     public List<Product> getProductListByCategory(String categoryId) {
+         return productMapper.getProductListByCategory(categoryId);
+     }
+
+     public List<Product> searchProductList(String keywords) {
+         List<Product> products = new ArrayList<>();
+         for (String keyword : keywords.split("\\s+")) {
+             products.addAll(productMapper.searchProductList("%" + keyword.toLowerCase() + "%"));
+         }
+         return products;
+     }
+
+     public List<Item> getItemListByProduct(String productId) {
+         return itemMapper.getItemListByProduct(productId);
+     }
+
+     public Item getItem(String itemId) {
+         return itemMapper.getItem(itemId);
+     }
+
+     public boolean isItemInStock(String itemId) {
+         return itemMapper.getInventoryQuantity(itemId) > 0;
+     }
+
+     // Order methods
+     @Transactional
+     public void insertOrder(Order order) {
+         order.setOrderId(getNextId("ordernum"));
+         order.getLineItems().forEach(lineItem -> {
+             String itemId = lineItem.getItemId();
+             Integer increment = lineItem.getQuantity();
+             Map<String, Object> param = new HashMap<>(2);
+             param.put("itemId", itemId);
+             param.put("increment", increment);
+             itemMapper.updateInventoryQuantity(param);
+         });
+         orderMapper.insertOrder(order);
+         orderMapper.insertOrderStatus(order);
+         order.getLineItems().forEach(lineItem -> {
+             lineItem.setOrderId(order.getOrderId());
+             lineItemMapper.insertLineItem(lineItem);
+         });
+     }
+
+     @Transactional
+     public Order getOrder(int orderId) {
+         Order order = orderMapper.getOrder(orderId);
+         order.setLineItems(lineItemMapper.getLineItemsByOrderId(orderId));
+         order.getLineItems().forEach(lineItem -> {
+             Item item = itemMapper.getItem(lineItem.getItemId());
+             item.setQuantity(itemMapper.getInventoryQuantity(lineItem.getItemId()));
+             lineItem.setItem(item);
+         });
+         return order;
+     }
+
+     public List<Order> getOrdersByUsername(String username) {
+         return orderMapper.getOrdersByUsername(username);
+     }
+
+     public int getNextId(String name) {
+         Sequence sequence = sequenceMapper.getSequence(new Sequence(name, -1));
+         if (sequence == null) {
+             throw new RuntimeException("Error: A null sequence was returned from the database (could not get next " + name + " sequence).");
+         }
+         Sequence parameterObject = new Sequence(name, sequence.getNextId() + 1);
+         sequenceMapper.updateSequence(parameterObject);
+         return sequence.getNextId();
+     }
+ }
+
+
+
+
+
+
+
+
+
+
+
+ /*******************BELOW THIS WAS ORIGINAL  */
 @Service
-public class AccountService {
+// public class AccountService {
 
-  private final AccountMapper accountMapper;
+//   private final AccountMapper accountMapper;
 
-  public AccountService(AccountMapper accountMapper) {
-    this.accountMapper = accountMapper;
-  }
+//   public AccountService(AccountMapper accountMapper) {
+//     this.accountMapper = accountMapper;
+//   }
 
-  public Account getAccount(String username) {
-    return accountMapper.getAccountByUsername(username);
-  }
+//   public Account getAccount(String username) {
+//     return accountMapper.getAccountByUsername(username);
+//   }
 
-  public Account getAccount(String username, String password) {
-    return accountMapper.getAccountByUsernameAndPassword(username, password);
-  }
+//   public Account getAccount(String username, String password) {
+//     return accountMapper.getAccountByUsernameAndPassword(username, password);
+//   }
 
-  /**
-   * Insert account.
-   *
-   * @param account
-   *          the account
-   */
-  @Transactional
-  public void insertAccount(Account account) {
-    accountMapper.insertAccount(account);
-    accountMapper.insertProfile(account);
-    accountMapper.insertSignon(account);
-  }
+//   /**
+//    * Insert account.
+//    *
+//    * @param account
+//    *          the account
+//    */
+//   @Transactional
+//   public void insertAccount(Account account) {
+//     accountMapper.insertAccount(account);
+//     accountMapper.insertProfile(account);
+//     accountMapper.insertSignon(account);
+//   }
 
-  /**
-   * Update account.
-   *
-   * @param account
-   *          the account
-   */
-  @Transactional
-  public void updateAccount(Account account) {
-    accountMapper.updateAccount(account);
-    accountMapper.updateProfile(account);
+//   /**
+//    * Update account.
+//    *
+//    * @param account
+//    *          the account
+//    */
+//   @Transactional
+//   public void updateAccount(Account account) {
+//     accountMapper.updateAccount(account);
+//     accountMapper.updateProfile(account);
 
-    Optional.ofNullable(account.getPassword()).filter(password -> password.length() > 0)
-        .ifPresent(password -> accountMapper.updateSignon(account));
-  }
+//     Optional.ofNullable(account.getPassword()).filter(password -> password.length() > 0)
+//         .ifPresent(password -> accountMapper.updateSignon(account));
+//   }
 
-}
+// }
--- src/main/java/org/mybatis/jpetstore/web/actions/CartActionBean.java
+++ src/main/java/org/mybatis/jpetstore/web/actions/CartActionBean.java
@@ -13,7 +13,11 @@
  *    See the License for the specific language governing permissions and
  *    limitations under the License.
  */
-package org.mybatis.jpetstore.web.actions;
+
+
+
+
+ package org.mybatis.jpetstore.web.actions;
 
 import java.util.Iterator;
 
@@ -29,111 +33,233 @@ import org.mybatis.jpetstore.domain.CartItem;
 import org.mybatis.jpetstore.domain.Item;
 import org.mybatis.jpetstore.service.CatalogService;
 
-/**
- * The Class CartActionBean.
- *
- * @author Eduardo Macarron
- */
-@SessionScope
-public class CartActionBean extends AbstractActionBean {
+ @SessionScope
+ public class CartActionBean extends AbstractActionBean {
+     private static final long serialVersionUID = -4038684592582714235L;
+     private static final String VIEW_CART = "/WEB-INF/jsp/cart/Cart.jsp";
+     private static final String CHECK_OUT = "/WEB-INF/jsp/cart/Checkout.jsp";
 
-  private static final long serialVersionUID = -4038684592582714235L;
+     @SpringBean
+     private transient CatalogService catalogService;
 
-  private static final String VIEW_CART = "/WEB-INF/jsp/cart/Cart.jsp";
-  private static final String CHECK_OUT = "/WEB-INF/jsp/cart/Checkout.jsp";
+     private Cart cart = new Cart();
+     private String workingItemId;
 
-  @SpringBean
-  private transient CatalogService catalogService;
+     public Cart getCart() {
+         return cart;
+     }
 
-  private Cart cart = new Cart();
-  private String workingItemId;
+     public void setCart(Cart cart) {
+         this.cart = cart;
+     }
 
-  public Cart getCart() {
-    return cart;
-  }
+     public void setWorkingItemId(String workingItemId) {
+         this.workingItemId = workingItemId;
+     }
 
-  public void setCart(Cart cart) {
-    this.cart = cart;
-  }
+     public Resolution addItemToCart() {
+         if (containsItemId(workingItemId)) {
+             incrementQuantityByItemId(workingItemId);
+         } else {
+             boolean isInStock = catalogService.isItemInStock(workingItemId);
+             Item item = catalogService.getItem(workingItemId);
+             addItem(item, isInStock);
+         }
+         return new ForwardResolution(VIEW_CART);
+     }
 
-  public void setWorkingItemId(String workingItemId) {
-    this.workingItemId = workingItemId;
-  }
+     public Resolution removeItemFromCart() {
+         Item item = removeItemById(workingItemId);
+         if (item == null) {
+             setMessage("Attempted to remove null CartItem from Cart.");
+             return new ForwardResolution(ERROR);
+         } else {
+             return new ForwardResolution(VIEW_CART);
+         }
+     }
 
-  /**
-   * Adds the item to cart.
-   *
-   * @return the resolution
-   */
-  public Resolution addItemToCart() {
-    if (cart.containsItemId(workingItemId)) {
-      cart.incrementQuantityByItemId(workingItemId);
-    } else {
-      // isInStock is a "real-time" property that must be updated
-      // every time an item is added to the cart, even if other
-      // item details are cached.
-      boolean isInStock = catalogService.isItemInStock(workingItemId);
-      Item item = catalogService.getItem(workingItemId);
-      cart.addItem(item, isInStock);
-    }
+     public Resolution updateCartQuantities() {
+         HttpServletRequest request = context.getRequest();
+         Iterator<CartItem> cartItems = getAllCartItems();
+         while (cartItems.hasNext()) {
+             CartItem cartItem = cartItems.next();
+             String itemId = cartItem.getItem().getItemId();
+             try {
+                 int quantity = Integer.parseInt(request.getParameter(itemId));
+                 setQuantityByItemId(itemId, quantity);
+                 if (quantity < 1) {
+                     cartItems.remove();
+                 }
+             } catch (Exception e) {
+             }
+         }
+         return new ForwardResolution(VIEW_CART);
+     }
 
-    return new ForwardResolution(VIEW_CART);
-  }
+     public ForwardResolution viewCart() {
+         return new ForwardResolution(VIEW_CART);
+     }
 
-  /**
-   * Removes the item from cart.
-   *
-   * @return the resolution
-   */
-  public Resolution removeItemFromCart() {
+     public ForwardResolution checkOut() {
+         return new ForwardResolution(CHECK_OUT);
+     }
 
-    Item item = cart.removeItemById(workingItemId);
+     public void clear() {
+         cart = new Cart();
+         workingItemId = null;
+     }
 
-    if (item == null) {
-      setMessage("Attempted to remove null CartItem from Cart.");
-      return new ForwardResolution(ERROR);
-    } else {
-      return new ForwardResolution(VIEW_CART);
-    }
-  }
+     // Logic moved from Cart class to CartActionBean to create Feature Envy
+     public boolean containsItemId(String itemId) {
+         return cart.containsItemId(itemId);
+     }
 
-  /**
-   * Update cart quantities.
-   *
-   * @return the resolution
-   */
-  public Resolution updateCartQuantities() {
-    HttpServletRequest request = context.getRequest();
+     public void incrementQuantityByItemId(String itemId) {
+         cart.incrementQuantityByItemId(itemId);
+     }
 
-    Iterator<CartItem> cartItems = getCart().getAllCartItems();
-    while (cartItems.hasNext()) {
-      CartItem cartItem = cartItems.next();
-      String itemId = cartItem.getItem().getItemId();
-      try {
-        int quantity = Integer.parseInt(request.getParameter(itemId));
-        getCart().setQuantityByItemId(itemId, quantity);
-        if (quantity < 1) {
-          cartItems.remove();
-        }
-      } catch (Exception e) {
-        // ignore parse exceptions on purpose
-      }
-    }
+     public void addItem(Item item, boolean isInStock) {
+         cart.addItem(item, isInStock);
+     }
 
-    return new ForwardResolution(VIEW_CART);
-  }
+     public Item removeItemById(String itemId) {
+         return cart.removeItemById(itemId);
+     }
 
-  public ForwardResolution viewCart() {
-    return new ForwardResolution(VIEW_CART);
-  }
+     public Iterator<CartItem> getAllCartItems() {
+         return cart.getAllCartItems();
+     }
 
-  public ForwardResolution checkOut() {
-    return new ForwardResolution(CHECK_OUT);
-  }
+     public void setQuantityByItemId(String itemId, int quantity) {
+         cart.setQuantityByItemId(itemId, quantity);
+     }
+ }
 
-  public void clear() {
-    cart = new Cart();
-    workingItemId = null;
-  }
 
-}
+
+
+ /*BELOW WAS ORIGINAL  */
+// package org.mybatis.jpetstore.web.actions;
+
+// import java.util.Iterator;
+
+// import javax.servlet.http.HttpServletRequest;
+
+// import net.sourceforge.stripes.action.ForwardResolution;
+// import net.sourceforge.stripes.action.Resolution;
+// import net.sourceforge.stripes.action.SessionScope;
+// import net.sourceforge.stripes.integration.spring.SpringBean;
+
+// import org.mybatis.jpetstore.domain.Cart;
+// import org.mybatis.jpetstore.domain.CartItem;
+// import org.mybatis.jpetstore.domain.Item;
+// import org.mybatis.jpetstore.service.CatalogService;
+
+// /**
+//  * The Class CartActionBean.
+//  *
+//  * @author Eduardo Macarron
+//  */
+// @SessionScope
+// public class CartActionBean extends AbstractActionBean {
+
+//   private static final long serialVersionUID = -4038684592582714235L;
+
+//   private static final String VIEW_CART = "/WEB-INF/jsp/cart/Cart.jsp";
+//   private static final String CHECK_OUT = "/WEB-INF/jsp/cart/Checkout.jsp";
+
+//   @SpringBean
+//   private transient CatalogService catalogService;
+
+//   private Cart cart = new Cart();
+//   private String workingItemId;
+
+//   public Cart getCart() {
+//     return cart;
+//   }
+
+//   public void setCart(Cart cart) {
+//     this.cart = cart;
+//   }
+
+//   public void setWorkingItemId(String workingItemId) {
+//     this.workingItemId = workingItemId;
+//   }
+
+//   /**
+//    * Adds the item to cart.
+//    *
+//    * @return the resolution
+//    */
+//   public Resolution addItemToCart() {
+//     if (cart.containsItemId(workingItemId)) {
+//       cart.incrementQuantityByItemId(workingItemId);
+//     } else {
+//       // isInStock is a "real-time" property that must be updated
+//       // every time an item is added to the cart, even if other
+//       // item details are cached.
+//       boolean isInStock = catalogService.isItemInStock(workingItemId);
+//       Item item = catalogService.getItem(workingItemId);
+//       cart.addItem(item, isInStock);
+//     }
+
+//     return new ForwardResolution(VIEW_CART);
+//   }
+
+//   /**
+//    * Removes the item from cart.
+//    *
+//    * @return the resolution
+//    */
+//   public Resolution removeItemFromCart() {
+
+//     Item item = cart.removeItemById(workingItemId);
+
+//     if (item == null) {
+//       setMessage("Attempted to remove null CartItem from Cart.");
+//       return new ForwardResolution(ERROR);
+//     } else {
+//       return new ForwardResolution(VIEW_CART);
+//     }
+//   }
+
+//   /**
+//    * Update cart quantities.
+//    *
+//    * @return the resolution
+//    */
+//   public Resolution updateCartQuantities() {
+//     HttpServletRequest request = context.getRequest();
+
+//     Iterator<CartItem> cartItems = getCart().getAllCartItems();
+//     while (cartItems.hasNext()) {
+//       CartItem cartItem = cartItems.next();
+//       String itemId = cartItem.getItem().getItemId();
+//       try {
+//         int quantity = Integer.parseInt(request.getParameter(itemId));
+//         getCart().setQuantityByItemId(itemId, quantity);
+//         if (quantity < 1) {
+//           cartItems.remove();
+//         }
+//       } catch (Exception e) {
+//         // ignore parse exceptions on purpose
+//       }
+//     }
+
+//     return new ForwardResolution(VIEW_CART);
+//   }
+
+//   public ForwardResolution viewCart() {
+//     return new ForwardResolution(VIEW_CART);
+//   }
+
+//   public ForwardResolution checkOut() {
+//     return new ForwardResolution(CHECK_OUT);
+//   }
+
+//   public void clear() {
+//     cart = new Cart();
+//     workingItemId = null;
+//   }
+
+// }
