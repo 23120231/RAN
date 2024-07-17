e166d56ea6bd3726c4ba9562e998af16738155eb
23120231 <23120231@studentmail.ul.ie>
2024-07-17 22:13:56 +0100

    Only god class so far removed feature envy
    Only god class so far removed feature envy

--- src/main/java/org/mybatis/jpetstore/web/actions/CartActionBean.java
+++ src/main/java/org/mybatis/jpetstore/web/actions/CartActionBean.java
@@ -17,7 +17,9 @@
 
 
 
- package org.mybatis.jpetstore.web.actions;
+
+ /*BELOW WAS ORIGINAL  */
+package org.mybatis.jpetstore.web.actions;
 
 import java.util.Iterator;
 
@@ -33,233 +35,111 @@ import org.mybatis.jpetstore.domain.CartItem;
 import org.mybatis.jpetstore.domain.Item;
 import org.mybatis.jpetstore.service.CatalogService;
 
- @SessionScope
- public class CartActionBean extends AbstractActionBean {
-     private static final long serialVersionUID = -4038684592582714235L;
-     private static final String VIEW_CART = "/WEB-INF/jsp/cart/Cart.jsp";
-     private static final String CHECK_OUT = "/WEB-INF/jsp/cart/Checkout.jsp";
+/**
+ * The Class CartActionBean.
+ *
+ * @author Eduardo Macarron
+ */
+@SessionScope
+public class CartActionBean extends AbstractActionBean {
 
-     @SpringBean
-     private transient CatalogService catalogService;
+  private static final long serialVersionUID = -4038684592582714235L;
 
-     private Cart cart = new Cart();
-     private String workingItemId;
+  private static final String VIEW_CART = "/WEB-INF/jsp/cart/Cart.jsp";
+  private static final String CHECK_OUT = "/WEB-INF/jsp/cart/Checkout.jsp";
 
-     public Cart getCart() {
-         return cart;
-     }
+  @SpringBean
+  private transient CatalogService catalogService;
 
-     public void setCart(Cart cart) {
-         this.cart = cart;
-     }
+  private Cart cart = new Cart();
+  private String workingItemId;
 
-     public void setWorkingItemId(String workingItemId) {
-         this.workingItemId = workingItemId;
-     }
+  public Cart getCart() {
+    return cart;
+  }
 
-     public Resolution addItemToCart() {
-         if (containsItemId(workingItemId)) {
-             incrementQuantityByItemId(workingItemId);
-         } else {
-             boolean isInStock = catalogService.isItemInStock(workingItemId);
-             Item item = catalogService.getItem(workingItemId);
-             addItem(item, isInStock);
-         }
-         return new ForwardResolution(VIEW_CART);
-     }
+  public void setCart(Cart cart) {
+    this.cart = cart;
+  }
 
-     public Resolution removeItemFromCart() {
-         Item item = removeItemById(workingItemId);
-         if (item == null) {
-             setMessage("Attempted to remove null CartItem from Cart.");
-             return new ForwardResolution(ERROR);
-         } else {
-             return new ForwardResolution(VIEW_CART);
-         }
-     }
+  public void setWorkingItemId(String workingItemId) {
+    this.workingItemId = workingItemId;
+  }
 
-     public Resolution updateCartQuantities() {
-         HttpServletRequest request = context.getRequest();
-         Iterator<CartItem> cartItems = getAllCartItems();
-         while (cartItems.hasNext()) {
-             CartItem cartItem = cartItems.next();
-             String itemId = cartItem.getItem().getItemId();
-             try {
-                 int quantity = Integer.parseInt(request.getParameter(itemId));
-                 setQuantityByItemId(itemId, quantity);
-                 if (quantity < 1) {
-                     cartItems.remove();
-                 }
-             } catch (Exception e) {
-             }
-         }
-         return new ForwardResolution(VIEW_CART);
-     }
+  /**
+   * Adds the item to cart.
+   *
+   * @return the resolution
+   */
+  public Resolution addItemToCart() {
+    if (cart.containsItemId(workingItemId)) {
+      cart.incrementQuantityByItemId(workingItemId);
+    } else {
+      // isInStock is a "real-time" property that must be updated
+      // every time an item is added to the cart, even if other
+      // item details are cached.
+      boolean isInStock = catalogService.isItemInStock(workingItemId);
+      Item item = catalogService.getItem(workingItemId);
+      cart.addItem(item, isInStock);
+    }
 
-     public ForwardResolution viewCart() {
-         return new ForwardResolution(VIEW_CART);
-     }
+    return new ForwardResolution(VIEW_CART);
+  }
 
-     public ForwardResolution checkOut() {
-         return new ForwardResolution(CHECK_OUT);
-     }
+  /**
+   * Removes the item from cart.
+   *
+   * @return the resolution
+   */
+  public Resolution removeItemFromCart() {
 
-     public void clear() {
-         cart = new Cart();
-         workingItemId = null;
-     }
+    Item item = cart.removeItemById(workingItemId);
 
-     // Logic moved from Cart class to CartActionBean to create Feature Envy
-     public boolean containsItemId(String itemId) {
-         return cart.containsItemId(itemId);
-     }
+    if (item == null) {
+      setMessage("Attempted to remove null CartItem from Cart.");
+      return new ForwardResolution(ERROR);
+    } else {
+      return new ForwardResolution(VIEW_CART);
+    }
+  }
 
-     public void incrementQuantityByItemId(String itemId) {
-         cart.incrementQuantityByItemId(itemId);
-     }
+  /**
+   * Update cart quantities.
+   *
+   * @return the resolution
+   */
+  public Resolution updateCartQuantities() {
+    HttpServletRequest request = context.getRequest();
 
-     public void addItem(Item item, boolean isInStock) {
-         cart.addItem(item, isInStock);
-     }
+    Iterator<CartItem> cartItems = getCart().getAllCartItems();
+    while (cartItems.hasNext()) {
+      CartItem cartItem = cartItems.next();
+      String itemId = cartItem.getItem().getItemId();
+      try {
+        int quantity = Integer.parseInt(request.getParameter(itemId));
+        getCart().setQuantityByItemId(itemId, quantity);
+        if (quantity < 1) {
+          cartItems.remove();
+        }
+      } catch (Exception e) {
+        // ignore parse exceptions on purpose
+      }
+    }
 
-     public Item removeItemById(String itemId) {
-         return cart.removeItemById(itemId);
-     }
+    return new ForwardResolution(VIEW_CART);
+  }
 
-     public Iterator<CartItem> getAllCartItems() {
-         return cart.getAllCartItems();
-     }
+  public ForwardResolution viewCart() {
+    return new ForwardResolution(VIEW_CART);
+  }
 
-     public void setQuantityByItemId(String itemId, int quantity) {
-         cart.setQuantityByItemId(itemId, quantity);
-     }
- }
+  public ForwardResolution checkOut() {
+    return new ForwardResolution(CHECK_OUT);
+  }
 
+  public void clear() {
+    cart = new Cart();
+    workingItemId = null;
+  }
 
-
-
- /*BELOW WAS ORIGINAL  */
-// package org.mybatis.jpetstore.web.actions;
-
-// import java.util.Iterator;
-
-// import javax.servlet.http.HttpServletRequest;
-
-// import net.sourceforge.stripes.action.ForwardResolution;
-// import net.sourceforge.stripes.action.Resolution;
-// import net.sourceforge.stripes.action.SessionScope;
-// import net.sourceforge.stripes.integration.spring.SpringBean;
-
-// import org.mybatis.jpetstore.domain.Cart;
-// import org.mybatis.jpetstore.domain.CartItem;
-// import org.mybatis.jpetstore.domain.Item;
-// import org.mybatis.jpetstore.service.CatalogService;
-
-// /**
-//  * The Class CartActionBean.
-//  *
-//  * @author Eduardo Macarron
-//  */
-// @SessionScope
-// public class CartActionBean extends AbstractActionBean {
-
-//   private static final long serialVersionUID = -4038684592582714235L;
-
-//   private static final String VIEW_CART = "/WEB-INF/jsp/cart/Cart.jsp";
-//   private static final String CHECK_OUT = "/WEB-INF/jsp/cart/Checkout.jsp";
-
-//   @SpringBean
-//   private transient CatalogService catalogService;
-
-//   private Cart cart = new Cart();
-//   private String workingItemId;
-
-//   public Cart getCart() {
-//     return cart;
-//   }
-
-//   public void setCart(Cart cart) {
-//     this.cart = cart;
-//   }
-
-//   public void setWorkingItemId(String workingItemId) {
-//     this.workingItemId = workingItemId;
-//   }
-
-//   /**
-//    * Adds the item to cart.
-//    *
-//    * @return the resolution
-//    */
-//   public Resolution addItemToCart() {
-//     if (cart.containsItemId(workingItemId)) {
-//       cart.incrementQuantityByItemId(workingItemId);
-//     } else {
-//       // isInStock is a "real-time" property that must be updated
-//       // every time an item is added to the cart, even if other
-//       // item details are cached.
-//       boolean isInStock = catalogService.isItemInStock(workingItemId);
-//       Item item = catalogService.getItem(workingItemId);
-//       cart.addItem(item, isInStock);
-//     }
-
-//     return new ForwardResolution(VIEW_CART);
-//   }
-
-//   /**
-//    * Removes the item from cart.
-//    *
-//    * @return the resolution
-//    */
-//   public Resolution removeItemFromCart() {
-
-//     Item item = cart.removeItemById(workingItemId);
-
-//     if (item == null) {
-//       setMessage("Attempted to remove null CartItem from Cart.");
-//       return new ForwardResolution(ERROR);
-//     } else {
-//       return new ForwardResolution(VIEW_CART);
-//     }
-//   }
-
-//   /**
-//    * Update cart quantities.
-//    *
-//    * @return the resolution
-//    */
-//   public Resolution updateCartQuantities() {
-//     HttpServletRequest request = context.getRequest();
-
-//     Iterator<CartItem> cartItems = getCart().getAllCartItems();
-//     while (cartItems.hasNext()) {
-//       CartItem cartItem = cartItems.next();
-//       String itemId = cartItem.getItem().getItemId();
-//       try {
-//         int quantity = Integer.parseInt(request.getParameter(itemId));
-//         getCart().setQuantityByItemId(itemId, quantity);
-//         if (quantity < 1) {
-//           cartItems.remove();
-//         }
-//       } catch (Exception e) {
-//         // ignore parse exceptions on purpose
-//       }
-//     }
-
-//     return new ForwardResolution(VIEW_CART);
-//   }
-
-//   public ForwardResolution viewCart() {
-//     return new ForwardResolution(VIEW_CART);
-//   }
-
-//   public ForwardResolution checkOut() {
-//     return new ForwardResolution(CHECK_OUT);
-//   }
-
-//   public void clear() {
-//     cart = new Cart();
-//     workingItemId = null;
-//   }
-
-// }
+}
